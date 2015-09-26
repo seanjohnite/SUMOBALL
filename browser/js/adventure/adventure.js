cf4d128a035e76ea5e5e88a2ae9bad41021bc325
Sean Johnston <sean.johnst@gmail.com>
2015-09-26 16:38:46 -0400

    starting adventuring

---
+++ browser/js/adventure/adventure.js
@@ -0,0 +1,16 @@
+app.config(function ($stateProvider) {
+    $stateProvider.state('adventure', {
+        url: '/adventure',
+        templateUrl: '/js/adventure/adventure.html',
+        controller: function ($scope, Images, Socket, Three, $interval, $rootScope) {
+            $scope.renderer;
+            $scope.scene;
+
+            $scope.init = function () {
+
+            }
+        }
+    });
+});
+
+
